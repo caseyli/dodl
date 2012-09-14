046b2dedd176aff4e71d52aa69d93cd9df2bbf3c
Casey Li <casey.li@gmail.com>
2012-09-14 16:04:49 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dodl::Application.config.secret_token = 'c53d916894873d693ce9cd1abd3662ef47e86f3d8cfb2d3fc1fb90d2b71be9fb7477c930a24a3ee2ceb12ca1e7ce803ec927f53d819977ccce2eca671ac30853'

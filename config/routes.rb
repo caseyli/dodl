7479a8601ab195c7807ee1da6e5d2f98ccf948a3
Mark Dabrowski <dabrowski.mark@gmail.com>
2012-10-02 21:01:58 -0400

    Got Devise implemented!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Dodl::Application.routes.draw do
+  
+  devise_for :users
+
+  get "pages/home"
+
+  root :to => "pages#home"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

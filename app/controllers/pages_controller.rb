1f271b304589ad799fdf4886fec0838449551b4d
Mark Dabrowski <dabrowski.mark@gmail.com>
2012-10-28 19:18:58 -0400

    Managing settings of dartboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
+    @settings = Setting.all
+    
+    @settings.each do |setting|
+      case setting.property
+      when "HighlightColor"
+        @highlight_clr = setting
+      when "DtColor1"
+        @dt_ring_clr = setting
+      when "DtColor2"
+        @dt_ring_clr_2 = setting
+      when "SectorColor1"
+        @sector_clr = setting
+      when "SectorColor2"
+        @sector_clr_2 = setting
+      end
+    end
   end
 end
---
+++ app/models/setting.rb
@@ -0,0 +1,3 @@
+class Setting < ActiveRecord::Base
+  attr_accessible :property, :value
+end
